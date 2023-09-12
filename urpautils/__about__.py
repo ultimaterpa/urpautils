93d5f28f82c8f0154ee9ec8ed2638102c95d9edc
anerold <60914831+anerold@users.noreply.github.com>
2023-09-12 10:13:55 +0200

    version 0.8.0

--- urpautils/__about__.py
+++ urpautils/__about__.py
@@ -1,7 +1,7 @@
 """Information about urpautils module."""
 __title__ = "urpautils"
 __description__ = "Module containig universal functions used with UltimateRPA robots"
-__version__ = "0.7.1"
+__version__ = "0.8.0"
 __author__ = "anerold"
 __author_email__ = "support@ultimaterpa.cz"
 __url__ = "https://github.com/ultimaterpa/urpautils"
